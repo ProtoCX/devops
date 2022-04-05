d1faa980063bbe95a97bdfb457b3e8f4fad0d65c
Albert Zhuang <albert@proto.cx>
2022-04-04 19:38:00 -0700

    Update startup-script.bash

--- startup-script.bash
+++ startup-script.bash
@@ -3,14 +3,14 @@ apt update
 
 apt -y install git zsh tmux htop curl python3.8 python3.8-venv python3-pip wget xauth x11-apps tig postgresql-client-12 jq gitk rsync vim 
 
-sudo apt-get install software-properties-common -y
-sudo add-apt-repository -y ppa:deadsnakes/ppa && sudo apt-get update
-sudo apt-get install -y python3.10 python3.10-venv
-sudo python3.10 -m ensurepip
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.10 2
+apt-get install software-properties-common -y
+add-apt-repository -y ppa:deadsnakes/ppa && apt-get update
+apt-get install -y python3.10 python3.10-venv
+python3.10 -m ensurepip
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2
+update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
+update-alternatives --install /usr/bin/python python /usr/bin/python3.10 2
 
 sudo apt install -y cloud-guest-utils 
 
