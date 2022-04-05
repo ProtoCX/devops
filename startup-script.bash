09190f87f65312f8bef3eea6a5dc3c0318e9a0b2
Albert Zhuang <albert@proto.cx>
2022-04-04 19:29:17 -0700

    Update startup-script.bash

--- startup-script.bash
+++ startup-script.bash
@@ -6,7 +6,7 @@ apt -y install git zsh tmux htop curl python3.8 python3.8-venv python3-pip wget 
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository -y ppa:deadsnakes/ppa && sudo apt-get update
 sudo apt-get install -y python3.10 python3.10-venv
-curl -sSL https://bootstrap.pypa.io/get-pip.py | sudo python3.10
+sudo python3.10 -m ensurepip
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2
 
