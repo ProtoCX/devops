c2842b325427b63b3892cd354596f0ce15d9d204
Albert Zhuang <albert@proto.cx>
2022-04-04 19:48:38 -0700

    Update startup-script.bash

--- startup-script.bash
+++ startup-script.bash
@@ -3,15 +3,6 @@ apt update
 
 apt -y install git zsh tmux htop curl python3.8 python3.8-venv python3-pip wget xauth x11-apps tig postgresql-client-12 jq gitk rsync vim 
 
-apt-get install software-properties-common -y
-add-apt-repository -y ppa:deadsnakes/ppa && apt-get update
-apt-get install -y python3.10 python3.10-venv
-python3.10 -m ensurepip
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2
-update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
-update-alternatives --install /usr/bin/python python /usr/bin/python3.10 2
-
 sudo apt install -y cloud-guest-utils 
 
 
