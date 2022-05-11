f011745b9a405422ba9fab0b11ac904b7c7cc709
Albert Zhuang <albert@proto.cx>
2022-05-10 19:07:16 -0700

    Update startup-script.bash

--- startup-script.bash
+++ startup-script.bash
@@ -1,21 +1,26 @@
 #!/bin/bash -x
 apt update
 
+echo "install basic packages"
 apt -y install git zsh tmux htop curl python3.8 python3.8-venv python3-pip wget xauth x11-apps tig postgresql-client-12 jq gitk rsync vim 
 
 
+echo "Install python 3.10"
 apt-get install software-properties-common -y
 add-apt-repository -y ppa:deadsnakes/ppa && apt-get update
 apt-get install -y python3.10 python3.10-venv
 python3.10 -m ensurepip
+curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2
 update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
 update-alternatives --install /usr/bin/python python /usr/bin/python3.10 2
 
+
+echo "Install gloud util"
 sudo apt install -y cloud-guest-utils 
 
-
+echo "Install Docker"
 apt-get remove docker docker-engine docker.io containerd runc
 apt-get install -y \
     apt-transport-https \
@@ -33,11 +38,14 @@ echo \
 apt update
 apt-get install -y docker-ce docker-ce-cli containerd.io
 
+echo "Install Docker-Compose"
 python3.10 -m pip install -U docker-compose
 
+echo "Install node and yarn"
 curl -fsSL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 apt-get install -y nodejs
 npm install --global yarn@1.22.15
 
+echo "Finish"
 apt --purge autoremove -y
 apt autoclean -y
